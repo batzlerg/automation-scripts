458db0808f0e9425fe46071bdfeb948fcb0fd163
Graham Batzler <batzler@pm.me>
2024-09-08 11:47:37 -0700

    basic coupling script

---
+++ homebridge/accessory-status-coupling.js
@@ -0,0 +1,46 @@
+const INITIATOR_SERVICE_NAME = 'Plant Lights';
+const RECEIVER_SERVICE_NAME = 'Plant Ceiling';
+
+automation.listen(function (event) {
+  const initiatorOnStatus = event.serviceCharacteristics.find((c) => c.type === "On");
+  const returnValue = {
+    serviceName: event.serviceName,
+    status: !!initiatorOnStatus
+      ? (initiatorOnStatus.value ? 'turned on' : 'turned off')
+      : null,
+    actionTaken: false,
+    message: ''
+    // uncomment for more involved debugging
+    // event: JSON.stringify(event),
+  };
+
+  if (event.serviceName !== INITIATOR_SERVICE_NAME) {
+    returnValue.message = `event not for ${INITIATOR_SERVICE_NAME}`;
+    return returnValue;
+  }
+
+  if (initiatorOnStatus) {
+    const receiverService = automation.services.find(
+      (s) => s.serviceName === RECEIVER_SERVICE_NAME,
+    );
+
+    if (!receiverService) {
+      returnValue.message = `could not find ${RECEIVER_SERVICE_NAME}, returned: ${JSON.stringify(receiverService)}`;
+      return returnValue;
+    }
+
+    const receiverOnStatus = receiverService.serviceCharacteristics.find((s) => s.type === "On");
+    if (!receiverOnStatus) {
+      returnValue.message = `could not find receiver onStatus, returned: ${JSON.stringify(receiverOnStatus)}`;
+      return returnValue;
+    }
+
+    automation.set(receiverService.uniqueId, receiverOnStatus.iid, initiatorOnStatus.value);
+
+    returnValue.actionTaken = true;
+    returnValue.message = 'success';
+    return returnValue;
+  }
+
+  return null;
+});
