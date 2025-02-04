eae53c4188271da6a59684e949a0747fcfbe0df5
graham <batzler@pm.me>
2025-02-03 18:41:24 -0800

    migrate servo-lightswitch to homeassistant, mostly refactor to use ArduinoJson

--- home-assistant/servo-lightswitch/servo-lightswitch-homeassistant.ino
+++ home-assistant/servo-lightswitch/servo-lightswitch-homeassistant.ino
@@ -3,6 +3,8 @@
 #include <ESP32Servo.h>
 #include <ArduinoJson.h>
 
+// TODO: finish migration to serialize instead of manual escaping
+
 // use an ESP-32 to set up a tiny web server attached to a servo,
 // and use that servo to physically switch a single pole home lightswitch
 // on and off. you can then configure this as a light switch in Home Assistant using
@@ -17,9 +19,9 @@ const char *WIFI_PASSWORD = "PASSWORD_GOES_HERE";
 
 // on Macbook you can do `networksetup -getinfo Wi-Fi` to get these values:
 const IPAddress staticIP(192, 168, 0, 194); // first three from gateway, last one you choose
-const IPAddress gateway(192, 168, 0, 1); // router IP
-const IPAddress subnet(255, 255, 255, 0); // subnet mask
-const IPAddress dns(192, 168, 0, 1); // router IP
+const IPAddress gateway(192, 168, 0, 1);    // router IP
+const IPAddress subnet(255, 255, 255, 0);   // subnet mask
+const IPAddress dns(192, 168, 0, 1);        // router IP
 
 WebServer server(80);
 
@@ -33,17 +35,20 @@ bool switchState = false;
 const int onPosition = 75;
 const int offPosition = 0;
 
-void logMessage(const String &message) {
+void logMessage(const String &message)
+{
   Serial.println(String(millis()) + "ms: " + message);
 }
 
-void setupWiFi() {
+void setupWiFi()
+{
   WiFi.mode(WIFI_STA);
   WiFi.disconnect(true); // disconnect and clear previous configs
   delay(100);
 
   // set static IP
-  if (!WiFi.config(staticIP, gateway, subnet, dns)) {
+  if (!WiFi.config(staticIP, gateway, subnet, dns))
+  {
     logMessage("Static IP Configuration Failed!");
   }
 
@@ -51,14 +56,16 @@ void setupWiFi() {
   logMessage("Connecting to WiFi...");
 
   int attempts = 0;
-  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+  while (WiFi.status() != WL_CONNECTED && attempts < 20)
+  {
     delay(500);
     Serial.print(".");
     attempts++;
   }
   Serial.println();
 
-  if (WiFi.status() == WL_CONNECTED) {
+  if (WiFi.status() == WL_CONNECTED)
+  {
     logMessage("Connected to WiFi");
     logMessage("IP: " + WiFi.localIP().toString());
     logMessage("Gateway: " + WiFi.gatewayIP().toString());
@@ -71,59 +78,91 @@ void setupWiFi() {
   }
 }
 
-void handleState() {
+void sendStateResponse()
+{
+  StaticJsonDocument<200> doc;
+  doc["state"] = switchState ? "ON" : "OFF";
+  String response;
+  serializeJson(doc, response);
+  server.send(200, "application/json", response);
+}
+
+void handleState()
+{
   logMessage("STATE endpoint hit");
   server.enableCORS(true);
-  
-  if (server.method() == HTTP_GET) {
-    // return current state in JSON format
-    StaticJsonDocument<200> doc;
-    doc["state"] = switchState ? "ON" : "OFF";
-    String response;
-    serializeJson(doc, response);
-    server.send(200, "application/json", response);
+
+  if (server.method() == HTTP_GET)
+  {
+    sendStateResponse();
   }
-  else if (server.method() == HTTP_POST) {
-    // parse incoming JSON
+  else if (server.method() == HTTP_POST)
+  {
     StaticJsonDocument<200> doc;
-    if (server.hasArg("plain")) {
+    if (server.hasArg("plain"))
+    {
       DeserializationError error = deserializeJson(doc, server.arg("plain"));
-      
-      if (!error) {
-        const char* state = doc["state"];
-        if (state) {
-          if (strcmp(state, "ON") == 0) {
+
+      if (!error)
+      {
+        const char *state = doc["state"];
+        if (state)
+        {
+          if (strcmp(state, "ON") == 0)
+          {
             switchState = true;
             switchServo.write(onPosition);
-            server.send(200, "application/json", "{\"state\":\"ON\"}");
+            sendStateResponse();
           }
-          else if (strcmp(state, "OFF") == 0) {
+          else if (strcmp(state, "OFF") == 0)
+          {
             switchState = false;
             switchServo.write(offPosition);
-            server.send(200, "application/json", "{\"state\":\"OFF\"}");
+            sendStateResponse();
           }
-          else {
-            server.send(400, "application/json", "{\"error\":\"Invalid state\"}");
+          else
+          {
+            StaticJsonDocument<200> errorDoc;
+            errorDoc["error"] = "Invalid state";
+            String errorResponse;
+            serializeJson(errorDoc, errorResponse);
+            server.send(400, "application/json", errorResponse);
           }
         }
       }
-      else {
-        server.send(400, "application/json", "{\"error\":\"Invalid JSON\"}");
+      else
+      {
+        StaticJsonDocument<200> errorDoc;
+        errorDoc["error"] = "Invalid JSON";
+        String errorResponse;
+        serializeJson(errorDoc, errorResponse);
+        server.send(400, "application/json", errorResponse);
       }
     }
-    else {
-      server.send(400, "application/json", "{\"error\":\"No data received\"}");
+    else
+    {
+      StaticJsonDocument<200> errorDoc;
+      errorDoc["error"] = "No data received";
+      String errorResponse;
+      serializeJson(errorDoc, errorResponse);
+      server.send(400, "application/json", errorResponse);
     }
   }
 }
 
-void handleNotFound() {
+void handleNotFound()
+{
   logMessage("404: " + server.uri());
   server.enableCORS(true);
-  server.send(404, "application/json", "{\"error\":\"Not found\"}");
+  StaticJsonDocument<200> doc;
+  doc["error"] = "Not found";
+  String response;
+  serializeJson(doc, response);
+  server.send(404, "application/json", response);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   logMessage("Starting up...");
 
@@ -145,11 +184,13 @@ void setup() {
   logMessage("HTTP server started");
 }
 
-void handleButton() {
+void handleButton()
+{
   static int lastButtonState = HIGH;
   int buttonState = digitalRead(bootButtonPin);
 
-  if (buttonState != lastButtonState) {
+  if (buttonState != lastButtonState)
+  {
     if (buttonState == LOW)
     {
       logMessage("Button press detected");
@@ -170,7 +211,8 @@ void handleButton() {
   }
 }
 
-void loop() {
+void loop()
+{
   static unsigned long lastCheck = 0;
 
   // run handlers
@@ -178,7 +220,8 @@ void loop() {
   handleButton();
 
   // keepalive wifi
-  if (millis() - lastCheck > 10000) { // 10 seconds
+  if (millis() - lastCheck > 10000)
+  { // 10 seconds
     lastCheck = millis();
     if (WiFi.status() != WL_CONNECTED)
     {
